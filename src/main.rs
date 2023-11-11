0f6d44bb64531c99b982addc961baa9fa8ece1bb
Ma902 <maxouschoumaker@gmail.com>
2023-11-11 16:18:56 +0100

    add color & help cmd & make array cmd

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::io;
+use colored::Colorize;
 
 fn main() {
     
-    //echo command
-    const CMD: &str = "echo ";
 
-    const EXIT: &str = ":q";
+    const CMD: [&str; 3] = ["echo ", ":q", "?"];
+
 
     loop {
 
@@ -16,13 +16,22 @@ fn main() {
         match io::stdin().read_line(&mut word) {
             Ok(_n) => {
                 // exit program
-                if word.contains(EXIT) {
+                if word.contains(CMD[1]) {
                     break;
                 }
 
+                if word.contains(CMD[2])  {
+                    if let Some(_word) = word.trim().strip_prefix(CMD[2]) {
+
+                        println!("{}           exit program\n{}[text]  repeat text\n{}            help command", CMD[1].blue(), CMD[0].blue(), CMD[2].blue());
+                        continue;
+                    }
+                }
+
+
                 //echo program
-                if word.contains(CMD)  {
-                    if let Some(word) = word.trim().strip_prefix(CMD) {
+                if word.contains(CMD[0])  {
+                    if let Some(word) = word.trim().strip_prefix(CMD[0]) {
                             println!("{}", word);
                         } else {
                             println!("command unknown!");
