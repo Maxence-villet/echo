8a123fcc88d55bb9272b49d9ee6181c8bd8d7832
Ma902 <maxouschoumaker@gmail.com>
2023-11-11 15:46:27 +0100

    loop and exit

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,38 @@
-use core::time;
 use std::io;
-use std::thread;
+
 fn main() {
     
     //echo command
     const CMD: &str = "echo ";
 
-    // input
-    let mut word = String::new();
-    
-    // check command
-    match io::stdin().read_line(&mut word) {
-        Ok(_n) => {
-            if word.contains(CMD)  {
-                if let Some(word) = word.trim().strip_prefix(CMD) {
-                        println!("{}", word);
-                    } else {
-                        println!("command unknown!");
-                    }
-            } else {
-                println!("command unknown!");
-            }
-        },
-        Err(error) => println!("error : {error}"),
-    }
+    const EXIT: &str = ":q";
 
-    let time_for_exit = time::Duration::from_secs(5);
-    thread::sleep(time_for_exit);
+    loop {
+
+        // input
+        let mut word = String::new();
+
+        // check command
+        match io::stdin().read_line(&mut word) {
+            Ok(_n) => {
+                // exit program
+                if word.contains(EXIT) {
+                    break;
+                }
+
+                //echo program
+                if word.contains(CMD)  {
+                    if let Some(word) = word.trim().strip_prefix(CMD) {
+                            println!("{}", word);
+                        } else {
+                            println!("command unknown!");
+                        }
+                } else {
+                    println!("command unknown!");
+                }
+            },
+            Err(error) => println!("error : {error}"),
+        }
+
+    }
 }
